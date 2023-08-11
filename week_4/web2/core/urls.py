cd6b1c26925f9c2267a9848da5874320340dc44e
Izam Mohammed <izamdeveloper1@gmail.com>
2023-08-11 09:49:15 +0530

    added some cache handle

--- week_4/web2/core/urls.py
+++ week_4/web2/core/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('', views.index, name='home'),
-    path('logout', views.logout, name='logout'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup')
+    
 ]
