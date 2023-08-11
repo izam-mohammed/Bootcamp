2e3386c7e98b0edffe010d16060c61ffd69fc130
Izam Mohammed <izamdeveloper1@gmail.com>
2023-08-11 09:49:15 +0530

    added some cache handle

--- week_4/web2/core/views.py
+++ week_4/web2/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.views.decorators.cache import never_cache
 
 # Create your views here.
 def login(request):
@@ -15,9 +17,10 @@ def login(request):
             messages.add_message(request, messages.WARNING, "invalid credentials")
     return render(request, 'login.html')
 
+@never_cache
 def index(request):
     if 'username' in request.session:
-        return render(request, 'index.html')
+        return render(request, 'index.html', context={'username': request.session['username']})
     return redirect(login)
 
 def logout(request):
@@ -27,3 +30,24 @@ def logout(request):
         except:
             pass
     return redirect(login)
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password != password2:
+            messages.add_message(request, messages.WARNING, 'Password not matching')
+        elif User.objects.filter(username=username):
+            messages.add_message(request, messages.WARNING, 'username is taken')
+        elif User.objects.filter(email=email):
+            messages.add_message(request, messages.WARNING, 'email is taken')
+        else:
+            user = User(username=username, email=email, password=password)
+            user.save()
+            request.session['username'] = username
+            return redirect('/')
+            
+    return render(request, 'signup.html')
